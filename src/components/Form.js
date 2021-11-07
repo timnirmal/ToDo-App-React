bb9bdbffb30c95f0fcbc2680e1b844ced1cbddc2
thimira nirmal <timnirmal@gmail.com>
2021-11-07 21:26:22 +0530

    Fix : Merging Sithum Code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,46 +1,46 @@
 import React from "react";
 
 function Form(props) {
-    const [form, setForm] = useState({
-        todo_item: "",
+  const [form, setForm] = useState({
+    todo_item: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    DATA.push({ id: "todo-" + DATA.length, name: form.todo_item, completed: false });
+    setForm({ todo_item: "" });
+  };
+
+  const updateForm = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
     });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        DATA.push({ id: "todo-" + DATA.length, name: form.todo_item, completed: false });
-        setForm({ todo_item: "" });
-    };
-
-    const updateForm = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value,
-        });
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2 className="label-wrapper">
-                    <label htmlFor="new-todo-input" className="label__lg">
-                        What needs to be done?
-                    </label>
-                </h2>
-                <input
-                    type="text"
-                    id="new-todo-input"
-                    className="input input__lg"
-                    name="todo_item"
-                    autoComplete="off"
-                    onChange={updateForm}
-                    value={form.todo_item}
-                />
-                <button type="submit" className="btn btn__primary btn__lg">
-                    Add
-                </button>
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="todo_item"
+          autoComplete="off"
+          onChange={updateForm}
+          value={form.todo_item}
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;

4e279491545307dd08d304aef0d8f9ca1439e9d7
thimira nirmal <timnirmal@gmail.com>
2021-11-07 16:00:21 +0530

    Add Unique Items

--- src/App.js
+++ src/App.js
@@ -46,54 +46,11 @@ function App(props) {
                 className="todo-list stack-large stack-exception"
                 aria-labelledby="list-heading"
             >
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-0" type="checkbox" defaultChecked={true} />
-                        <label className="todo-label" htmlFor="todo-0">
-                            Eat
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Eat</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete <span className="visually-hidden">Eat</span>
-                        </button>
-                    </div>
-                </li>
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-1" type="checkbox" />
-                        <label className="todo-label" htmlFor="todo-1">
-                            Sleep
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Sleep</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete <span className="visually-hidden">Sleep</span>
-                        </button>
-                    </div>
-                </li>
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-2" type="checkbox" />
-                        <label className="todo-label" htmlFor="todo-2">
-                            Repeat
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Repeat</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete <span className="visually-hidden">Repeat</span>
-                        </button>
-                    </div>
-                </li>
+                <Todo name="Eat" completed={true} id="todo-1"/>
+                <Todo name="Code" completed={false} id="todo-2"/>
+                <Todo name="Sleep" completed={false} id="todo-3"/>
+                <Todo name="Repeat" completed={false} id="todo-4"/>
+
             </ul>
         </div>
     );
